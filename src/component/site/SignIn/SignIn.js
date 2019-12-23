cb31e010e6154856d223047e2165ab2cc07faf21
Szymon Franczak <franczakszymon@gmail.com>
2019-12-23 13:56:13 +0100

    create SignIn page(basic)

--- src/component/site/SignIn/SignIn.js
+++ src/component/site/SignIn/SignIn.js
@@ -1,13 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+
 
 export class SignIn extends Component {
+    style={
+        email:'',
+        password:''
+
+    }
+    get formStyle(){
+        return({
+            display: "flex",
+            flexDirection: "column",
+            alignItem: "center",
+            justifyContent: "center",
+            position: "absolute",
+            left: "50%",
+            width: "30%",
+            transform: "translate(-50%,-50%)",
+            top: "50%"
+        })
+    }
+
+    get divStyle (){
+        return({
+            position: 'relative',
+            height: "90vh"
+        })
+    }
+
+    get buttonStyle(){
+        return({
+            marginTop: '1rem'
+        })
+    }
+
+    handleOnChange=(e)=>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
     render() {
         return (
-            <div>
-                <form>
-                    <input type='email' name='email' placeholder='wpisz swój email'/>
-                    <input type='password' name='password' placeholder = 'wpisz swoje hasło'/>
-                    <button type='submit'> Zaloguj </button>
+            <div className="SignIn row" style={this.divStyle} >
+                <form style={this.formStyle}>
+                    <TextField type="email" label="e-mail" name="email" onChange={this.handleOnChange}/>
+                    <TextField type="password" label="Hasło" name="password" onChange={this.handleOnChange}/>
+                    <Button
+                        style={this.buttonStyle}
+                        variant="contained"
+                        color="primary"
+                        className='signUpBtn'
+                        endIcon={<SendIcon>send</SendIcon>}
+                    > 
+                    Zaloguj
+                    </Button>
                 </form>
             </div>
         )
